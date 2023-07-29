2074013ac471c8c716732bf24aea5ec300ee0f6a
fetaderr <abdelfetah.derr@gmail.com>
2023-07-29 01:28:54 +0100

    Update FPSCharacterController.cpp
    Implemented function to allow adding ammo to ammoType through blueprints.

--- Source/FPSCore/Private/FPSCharacterController.cpp
+++ Source/FPSCore/Private/FPSCharacterController.cpp
@@ -1,3 +1,8 @@
 // Copyright 2022 Ellie Kelemen. All Rights Reserved.
 
-#include "FPSCharacterController.h"
+#include "FPSCharacterController.h"https://github.com/fetaderr/FPSCore/tree/patch-1
+
+void AFPSCharacterController::SetAmmoTypeValue(int32 AmmoCount, EAmmoType AmmoType)
+{
+    AmmoMap[AmmoType] += AmmoCount;
+}
