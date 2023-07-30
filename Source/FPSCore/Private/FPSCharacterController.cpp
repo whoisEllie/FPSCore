700e3559a0abaeccbe123bad6cd4159f5fb8bd9b
fetaderr <abdelfetah.derr@gmail.com>
2023-07-30 22:03:16 +0100

    Update FPSCharacterController.cpp
    Removed random link

--- Source/FPSCore/Private/FPSCharacterController.cpp
+++ Source/FPSCore/Private/FPSCharacterController.cpp
@@ -1,6 +1,6 @@
 // Copyright 2022 Ellie Kelemen. All Rights Reserved.
 
-#include "FPSCharacterController.h"https://github.com/fetaderr/FPSCore/tree/patch-1
+#include "FPSCharacterController.h"
 
 void AFPSCharacterController::SetAmmoTypeValue(int32 AmmoCount, EAmmoType AmmoType)
 {
